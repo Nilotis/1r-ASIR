ae7abdab9e8e287be6774be2a7de2f3c0dc8ad31
Nil Carmona Quesada <110260550+Nilotis@users.noreply.github.com>
2022-07-30 00:10:33 +0200

    Add files via upload

---
+++ UF3/Exercici 4/literales.py
@@ -0,0 +1,13 @@
+ROUTE = "./files/"
+HEADER = "ID\t\t\t|\tNom\t\t|\tCognom\t|\tEdat\t|\tInterés\t\t|\n" \
+         "--------------------------------------------------------------------- "
+EXT = ".csv"
+MSG1 = "Introdueix el teu nom: "
+MSG2 = "Introdueix el teu cognom: "
+MSG3 = "Introdueix la teva edat: "
+MSG4 = "Digues de quina tecnologia estás interessat:\n 1. Servidors\n 2. 5G\n 3. BIG DATA\n"
+MSG5 = "Vols continuar introduïnt registres [1-Sí / 0-No]: "
+MSG6 = "Com s'anomenarà el fitxer: "
+INT1 = "Servidors"
+INT2 = "5G"
+INT3 = "BIG DATA"
